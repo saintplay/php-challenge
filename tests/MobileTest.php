2dc3d7bbd8f8174a32aa36bbe257447949dec494
Diego Jara Palomino <saintplay96@gmail.com>
2020-07-21 18:34:49 -0500

    fix errors

--- tests/MobileTest.php
+++ tests/MobileTest.php
@@ -4,13 +4,28 @@ namespace Tests;
 
 use Mockery as m;
 use PHPUnit\Framework\TestCase;
+use \Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+
+
+use App\Mobile;
 
 class MobileTest extends TestCase
 {
+	use MockeryPHPUnitIntegration;
+	
+	// public function tearDown() {
+	// 	m::close();
+	// }
 	
 	/** @test */
 	public function it_returns_null_when_name_empty()
 	{
+		$provider = m::mock('App\Interfaces\CarrierInterface');
+		$provider->allows()
+			->makeCall()
+			->andReturn(true);
+
+
 		$mobile = new Mobile($provider);
 
 		$this->assertNull($mobile->makeCallByName(''));
